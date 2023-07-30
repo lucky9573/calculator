be598f3e072325760c14912966dd1b7fb37a84b8
likhith <kumarlikhith675@gmail.com>
2023-07-30 11:33:34 +0530

    update

--- src/script.js
+++ src/script.js
@@ -86,13 +86,13 @@ class Calculator {
 }
 
 
-const numberButtons = document.querySelectorAll('[data-number]')
-const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelector('[data-equals]')
-const deleteButton = document.querySelector('[data-delete]')
-const allClearButton = document.querySelector('[data-all-clear]')
-const previousOperandTextElement = document.querySelector('[data-previous-operand]')
-const currentOperandTextElement = document.querySelector('[data-current-operand]')
+const numberButtons = document.querySelectorAll('#number')
+const operationButtons = document.querySelectorAll('#operation')
+const equalsButton = document.querySelector('#equals')
+const deleteButton = document.querySelector('#delete')
+const allClearButton = document.querySelector('#all-clear')
+const previousOperandTextElement = document.querySelector('#previous-operand')
+const currentOperandTextElement = document.querySelector('#current-operand')
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 numberButtons.forEach(button => {
